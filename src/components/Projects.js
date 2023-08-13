a311547c4fbd5846337af7ae99a356333a6a8696
Jeison Novoa <106498984+JeisonNovoa@users.noreply.github.com>
2023-08-13 03:09:11 -0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -125,7 +125,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2 alt="color"}></img>
     </section>
   );
 };
